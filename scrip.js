5623d20afb846d8c07e4723d2f322c27a9bbde63
Lubos Rendek <web@linuxconfig.org>
2021-02-10 12:30:07 +0100

    version 3

--- scrip.js
+++ scrip.js
@@ -1,24 +1,25 @@
-let apicall = function(ville) {
-    let url = `https://restcountries.eu/rest/v2/all?fbclid=IwAR1-hadl5EpXh6sWm7hCQUadlc2Ro_VfFo0br_zdx8R3DFObToPkRazQOUo&name=${ville}`;
-    fetch(url).then((response) =>
-        response.json()
-        .then((data) => {
-            console.log(data)
-                //for (let i = 0; i < data.results.length; i++)
-            document.querySelector('#city').innerHTML = data.name;
-            document.querySelector('#capital').innerHTML = data.capital;
-            document.querySelector('#demonym').innerHTML = data.demonym;
+const searchForm = document.querySelector('form');
+const SearchResultDiv = document.querySelector('.serach-result');
+let searchQuery = '';
+searchForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    searchQuery = e.target.querySelector('input').value;
+    fetchApi();
+});
+async function fetchApi() {
+    const url = `https://restcountries.eu/rest/v2/name/${searchQuery}?fbclid=IwAR1-hadl5EpXh6sWm7hCQUadlc2Ro_VfFo0br_zdx8R3DFObToPkRazQOUo&q=${searchQuery}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    for (let i = 0; i < 250; i++) {
+        document.querySelector('#city').innerHTML = data[i].name;
+        document.querySelector('#capital').innerHTML = data[i].capital;
+        document.querySelector('#demonym').innerHTML = data[i].demonym;
+        document.querySelector('#languages').innerHTML = data[i].language;
+        document.querySelector('#region"').innerHTML = data[i].region;
+        document.querySelector('#population').innerHTML = data[i].population;
 
-            document.querySelector('#nativeName').innerHTML = data.nativeName;
-            document.querySelector('#population').innerHTML = data.population;
-            document.querySelector('#region"').innerHTML = data.region;
-        })
-        .catch(err => console.log('Erreur:' + err)))
 
+    }
 
 }
-document.querySelector('form').addEventListener("submit", function(e) {
-    e.preventDefault();
-    let search = document.querySelector('#inputCountry').Value;
-    apicall(search);
-});
